902f397e01c17b60c4d5e7891b0daa8e5b1dde83
sunjus <sunju.shin1108@gmail.com>
2021-08-09 19:20:13 +0300

    Added scrollTo

--- js/index.js
+++ js/index.js
@@ -32,3 +32,27 @@ window.addEventListener("scroll", () => {
     navBar.classList.remove("fix-nav");
   }
 });
+
+//Scroll To
+const links = [...document.querySelectorAll(".scroll-link")];
+
+links.map((link) => {
+  if (!link) return;
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const id = e.target.getAttribute("href").slice(1);
+
+    const el = document.getElementsById(id);
+    let position = el.offsetTop - navHeight;
+
+    window.scrollTo({
+      top: position,
+      left: 0,
+    });
+
+    menu.classList.remove("show");
+    document.body.classList.remove("show");
+    navBar.classList.remove("show");
+  });
+});
